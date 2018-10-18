4a96246ea0db9f1bd165b79f880501612bfa20f6
Luke Ehresman <luke@ehresman.org>
2018-10-17 20:12:06 -0400

    committed a basic fontawesome example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { faPlusCircle, faCircle, faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div style={{margin: 'auto', width: 600, borderRadius: 5, background: '#efefef', padding: 30, fontFamily: 'Georgia'}}>
+        <p>
+          <FontAwesomeIcon icon={faCheckCircle}/>{' '}
+          A simple example of an inline icon.
+        </p>
+
+        <p style={{color: '#c00', fontSize: 20}}>
+          <FontAwesomeIcon icon={faCheckCircle}/>{' '}
+          An example of the icon picking up the styles of the container.
+        </p>
+
+        <p style={{fontSize: 32}}>
+          <span className="fa-layers">
+            <FontAwesomeIcon icon={faCircle} style={{color: '#0c0'}}/>
+            <FontAwesomeIcon icon={faPlusCircle} style={{color: '#00f'}}/>
+          </span>{' '}
+          An example of composed icons.
+        </p>
       </div>
     );
   }
